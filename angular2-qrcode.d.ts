b0283df26812d766f119f31c097a10ccea080bf6
Justin Miller <superiorjt@gmail.com>
2017-02-07 11:29:50 -0500

    Implement qrious and update version number

--- angular2-qrcode.d.ts
+++ angular2-qrcode.d.ts
@@ -2,10 +2,16 @@ import { ElementRef, OnChanges, SimpleChanges } from '@angular/core';
 export declare class QRCodeComponent implements OnChanges {
     private elementRef;
     ngOnChanges(changes: SimpleChanges): void;
-    data: string;
-    size: number;
-    type: number;
+    background: string;
+    backgroundAlpha: number;
+    foreground: string;
+    foregroundAlpha: number;
     level: string;
+    mime: string;
+    padding: number;
+    size: number;
+    value: string;
+    canvas: boolean;
     constructor(elementRef: ElementRef);
     generate(): void;
 }
